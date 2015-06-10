a16b8769e612c5f0efc8f41e26f3562e1436afcd
dwayne Park <gregor77@naver.com>
2015-06-10 14:01:32 +0000

    led control bright sample

---
+++ code/gpio_basic/led_control_bright.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+
+led_pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+
+pwm_led = GPIO.PWM(led_pin, 500)
+pwm_led.start(100)
+
+while True:
+  duty_str = raw_input("Enter Brightness (0 to 100):")
+  duty = int(duty_str)
+
+  if duty > 100:
+     print("wrong input value.")
+  else:
+     pwm_led.ChangeDutyCycle(duty)
+
+ 
+  end_key = raw_input(" - Stop to Blink LED, Please enter the 'end' : ")
+  if end_key == "end":
+     break
+
+
+GPIO.cleanup()
