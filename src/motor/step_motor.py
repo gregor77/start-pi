10588718fbb7df4a891b74e859c9c1a21345b14b
jinsholic77@gmail.com <ace21315!>
2018-12-24 11:11:26 +0900

    Update sequence to rotate in clockwise direction with 28byj-48-stepper-motor

--- src/motor/step_motor.py
+++ src/motor/step_motor.py
@@ -5,12 +5,12 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-AIN1 = 15
-BIN1 = 16
-AIN2 = 18
-BIN2 = 22
+AIN1 = 15   # yellow
+BIN1 = 16   # orange
+AIN2 = 18   # blue
+BIN2 = 22   # pink
 
-sig = [[0, 0, 1, 1, 1, 1, 1, 0], [1, 0, 0, 0, 1, 1, 1, 1], [1, 1, 1, 0, 0, 0, 1, 1], [1, 1, 1, 1, 1, 0, 0, 0]]
+sig = [[1, 0, 0, 0, 1, 1, 1, 1], [0, 0, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1]]
 
 step = 100
 
@@ -24,14 +24,14 @@ GPIO.setup(BIN2, GPIO.OUT, initial=GPIO.LOW)
 try:
     while 1:
         for cnt in range(0, step):
+            cnt = cnt % 4
+
             GPIO.output(AIN1, sig[0][cnt])
             GPIO.output(BIN1, sig[1][cnt])
             GPIO.output(AIN2, sig[2][cnt])
             GPIO.output(BIN2, sig[3][cnt])
 
             time.sleep(0.01)
-
-            cnt = cnt % 4
 
         dir = dir * -1
 except KeyboardInterrupt:
